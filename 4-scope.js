360d8a8ec1e17aa7d842a364173b4fd4424077d9
vmarialuzm <vmarialuzm@gmail.com>
2023-08-09 11:57:03 -0500

    bases de javascript

---
+++ 4-scope.js
@@ -0,0 +1,11 @@
+var miNombre = "Diego"
+
+function nombre() {
+    var miApellido = "De Granda"; 
+    return miNombre + " " + miApellido 
+}
+
+nombre();
+
+console.log(miNombre);
+console.log(miApellido);
---
+++ 6-coercion.js
@@ -0,0 +1,9 @@
+var a = 20;
+var b = a + "";
+var c = String(a);
+
+console.log(c)
+
+var d = Number(c)
+
+typeof d;
